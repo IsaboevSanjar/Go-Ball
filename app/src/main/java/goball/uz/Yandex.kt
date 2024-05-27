51c1b0c9489f7f15ef0f7adbfba1eba9317463de
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-05-27 13:57:42 +0500

    Marker is working very fine

--- app/src/main/java/goball/uz/Yandex.kt
+++ app/src/main/java/goball/uz/Yandex.kt
@@ -1,5 +1,8 @@
 package goball.uz
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -23,9 +26,20 @@ class Yandex : AppCompatActivity() {
         ).show()
         true
     }
-    private val lat=41.2995
-    private val long=69.2401
+    private val lat = 41.2995
+    private val long = 69.2401
 
+    val locations = listOf(
+        Point(41.2995, 69.2501),
+        Point(41.2995, 69.2401),
+        Point(41.2900, 69.2401),
+        Point(41.2325, 69.2401),
+        Point(41.3100, 69.2300),
+        Point(41.3150, 69.2350),
+        Point(41.3200, 69.2510),
+    )
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         MapKitFactory.setApiKey("42c1c9b7-5b9f-4fc3-92f0-efcc45ec8dd6")
         super.onCreate(savedInstanceState)
@@ -37,14 +51,23 @@ class Yandex : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         mapView = findViewById(R.id.mapview)
         showMap(mapView)
-        val imageProvider = ImageProvider.fromResource(this, R.drawable.price_green)
-        val placeMark = mapView.mapWindow.map.mapObjects.addPlacemark().apply {
-            geometry = POINT
-            setIcon(imageProvider)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.stadium_marker)
+        // Resize the bitmap to a desired size (adjust width and height as needed)
+        val desiredWidth = 120// Adjust width in pixels (e.g., 48, 32)
+        val desiredHeight = 120 // Adjust height in pixels (e.g., 48, 32)
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmap!!, desiredWidth, desiredHeight, false)
+        val imageProvider = ImageProvider.fromBitmap(scaledBitmap)
+        locations.forEach { location ->
+            val placeMark = mapView.mapWindow.map.mapObjects.addPlacemark().apply {
+                geometry = location
+                setIcon(imageProvider)
+            }
+            // Optionally add tap listener for each placemark
+            placeMark.addTapListener(placeMarkTapListener)
         }
-        placeMark.addTapListener(placeMarkTapListener)
 
     }
 
@@ -70,6 +93,7 @@ class Yandex : AppCompatActivity() {
         MapKitFactory.getInstance().onStop()
         super.onStop()
     }
+
     companion object {
         private val POINT = Point(41.2995, 69.2401)
         private val POSITION = CameraPosition(
@@ -78,5 +102,6 @@ class Yandex : AppCompatActivity() {
             0.0f,
             30.0f
         )
+
     }
 }
