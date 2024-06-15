0dd39799dc0a323d78c3f8e316dcc48df4712f45
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-06-15 11:58:03 +0500

    Some design part in Redmi

--- app/src/main/java/goball/uz/ComposeActivity.kt
+++ app/src/main/java/goball/uz/ComposeActivity.kt
@@ -1,7 +1,6 @@
 package goball.uz
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -12,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.core.app.ActivityCompat
 import dagger.hilt.android.AndroidEntryPoint
 import goball.uz.ui.theme.GoBallTheme
 
