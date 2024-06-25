a5d3de1dfc784464721ccc29beba129a61b67daa
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-06-25 12:10:25 +0500

    Design par tof the LocationSelectionScreen

--- app/src/main/java/goball/uz/screens/mystadium/LocationSelectionScreen.kt
+++ app/src/main/java/goball/uz/screens/mystadium/LocationSelectionScreen.kt
@@ -3,14 +3,24 @@ package goball.uz.screens.mystadium
 import android.graphics.PointF
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -23,6 +33,7 @@ import goball.uz.R
 
 class LocationSelectionScreen(private val onLocationSelected: (latitude: Double, longitude: Double) -> Unit) :
     Screen {
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
         val context = LocalContext.current
@@ -53,7 +64,7 @@ class LocationSelectionScreen(private val onLocationSelected: (latitude: Double,
                 MapKitFactory.getInstance().onStop()
             }
         }
-        mapView?.let {map->
+        mapView?.let { map ->
             LaunchedEffect(map) {
                 map.onStart()
             }
@@ -62,7 +73,11 @@ class LocationSelectionScreen(private val onLocationSelected: (latitude: Double,
                 com.yandex.mapkit.Animation(com.yandex.mapkit.Animation.Type.SMOOTH, 0f),
                 null
             )
-            Column(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
                 Box(modifier = Modifier.weight(1f)) {
                     mapView?.let { view ->
                         AndroidView(factory = { view }, modifier = Modifier.fillMaxSize())
@@ -76,7 +91,56 @@ class LocationSelectionScreen(private val onLocationSelected: (latitude: Double,
                             .size(70.dp)
                             .padding(bottom = 8.dp)
                     )
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .background(color = Color.Transparent)
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 10.dp, start = 16.dp, end = 16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Card(
+                            onClick = { /*TODO*/ },
+                            shape = RoundedCornerShape(50.dp),
+                            modifier = Modifier.size(50.dp),
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.background
+                            ),
+                            elevation = CardDefaults.cardElevation(
+                                defaultElevation = 10.dp
+                            ),
+                        ) {
+                            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.arrow_back),
+                                    contentDescription = "Back Icon",
+                                    modifier = Modifier.size(25.dp)
+                                )
+                            }
+                        }
+                        Card(
+                            onClick = { /*TODO*/ },
+                            shape = RoundedCornerShape(50.dp),
+                            modifier = Modifier.size(50.dp),
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.background
+                            ),
+                            elevation = CardDefaults.cardElevation(
+                                defaultElevation = 10.dp
+                            ),
+                        ) {
+                            Box(contentAlignment = Alignment.Center ,modifier = Modifier.fillMaxSize()) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.current_location),
+                                    contentDescription = "Back Icon",
+                                    modifier = Modifier.size(25.dp)
+                                )
+                            }
+                        }
+                    }
+
                 }
+
                 Button(
                     onClick = {
                         mapView?.let {
@@ -86,9 +150,13 @@ class LocationSelectionScreen(private val onLocationSelected: (latitude: Double,
                     },
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp)
+                        .padding(bottom = 30.dp, top = 10.dp, start = 10.dp, end = 10.dp)
+                        .background(color = MaterialTheme.colorScheme.background),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.primary)
+                    )
                 ) {
-                    Text("Tanlash")
+                    Text(text = "Tanlash", style = MaterialTheme.typography.titleMedium, modifier = Modifier.padding(11.dp))
                 }
             }
         }
