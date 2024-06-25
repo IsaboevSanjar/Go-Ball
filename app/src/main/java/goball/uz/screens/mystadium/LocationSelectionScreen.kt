1318bc6292f099cf82cb047b77afbe9ff7a6477b
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-06-25 13:15:19 +0500

    focusing on the user current location

--- app/src/main/java/goball/uz/screens/mystadium/LocationSelectionScreen.kt
+++ app/src/main/java/goball/uz/screens/mystadium/LocationSelectionScreen.kt
@@ -1,7 +1,15 @@
 package goball.uz.screens.mystadium
 
-import android.graphics.PointF
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -11,21 +19,23 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
@@ -34,28 +44,57 @@ import goball.uz.R
 
 class LocationSelectionScreen(private val onLocationSelected: (latitude: Double, longitude: Double) -> Unit) :
     Screen {
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
         val context = LocalContext.current
         var mapView by remember { mutableStateOf<MapView?>(null) }
-        DisposableEffect(Unit) {
-            MapKitFactory.initialize(context)
-            mapView = MapView(context)
+        val fusedLocationProviderClient =
+            remember { LocationServices.getFusedLocationProviderClient(context) }
 
-            onDispose {
-                mapView?.onStop()
-                MapKitFactory.getInstance().onStop()
+        // Permissions request launcher
+        val requestPermissionLauncher = rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+            ) {
+                getUserLocation(fusedLocationProviderClient, context, mapView)
+            } else {
+                Toast.makeText(context, "Location permission denied", Toast.LENGTH_SHORT).show()
             }
         }
 
-        // Remember and initialize Yandex MapKit
+        // Request location permissions
+        val requestLocationPermission = {
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                getUserLocation(fusedLocationProviderClient, context, mapView)
+            } else {
+                requestPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
+            }
+        }
+
+        // Initialize Yandex MapKit
         DisposableEffect(Unit) {
             MapKitFactory.initialize(context)
             mapView = MapView(context)
             mapView?.let {
-                it.map.move(
+                it.mapWindow.map.move(
                     CameraPosition(Point(41.315486, 69.260827), 14.0f, 0.0f, 0.0f),
                     com.yandex.mapkit.Animation(com.yandex.mapkit.Animation.Type.SMOOTH, 0f),
                     null
@@ -66,6 +105,7 @@ class LocationSelectionScreen(private val onLocationSelected: (latitude: Double,
                 MapKitFactory.getInstance().onStop()
             }
         }
+
         mapView?.let { map ->
             LaunchedEffect(map) {
                 map.onStart()
@@ -124,7 +164,7 @@ class LocationSelectionScreen(private val onLocationSelected: (latitude: Double,
                             }
                         }
                         Card(
-                            onClick = { /*TODO*/ },
+                            onClick = { requestLocationPermission() },
                             shape = RoundedCornerShape(50.dp),
                             modifier = Modifier.size(50.dp),
                             colors = CardDefaults.cardColors(
@@ -140,7 +180,7 @@ class LocationSelectionScreen(private val onLocationSelected: (latitude: Double,
                             ) {
                                 Image(
                                     painter = painterResource(id = R.drawable.current_location),
-                                    contentDescription = "Back Icon",
+                                    contentDescription = "Current Location Icon",
                                     modifier = Modifier.size(25.dp)
                                 )
                             }
@@ -172,6 +212,38 @@ class LocationSelectionScreen(private val onLocationSelected: (latitude: Double,
                 }
             }
         }
+    }
 
+    @SuppressLint("MissingPermission")
+    private fun getUserLocation(
+        fusedLocationProviderClient: FusedLocationProviderClient,
+        context: Context,
+        mapView: MapView?
+    ) {
+        if ((context.getSystemService(Context.LOCATION_SERVICE) as LocationManager).isProviderEnabled(
+                LocationManager.GPS_PROVIDER
+            ) ||
+            (context.getSystemService(Context.LOCATION_SERVICE) as LocationManager).isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+            )
+        ) {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    val userLocationPoint = Point(location.latitude, location.longitude)
+                    mapView?.mapWindow?.map?.move(
+                        CameraPosition(userLocationPoint, 15.0f, 0.0f, 0.0f),
+                        com.yandex.mapkit.Animation(com.yandex.mapkit.Animation.Type.SMOOTH, 1f),
+                        null
+                    )
+                } else {
+                    Toast.makeText(context, "Location is null", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(context, "Manzilni olib bo'lmadi", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(context, "Iltimos joylashuvingizni yoqing!", Toast.LENGTH_SHORT).show()
+            context.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
     }
 }
