9d2713855633a8b0f23e1831370feb9e66012f21
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-06-28 15:27:24 +0500

    UI of About us is kind of ready

--- app/src/main/java/goball/uz/screens/drawers/AboutUsScreen.kt
+++ app/src/main/java/goball/uz/screens/drawers/AboutUsScreen.kt
@@ -3,10 +3,18 @@ package goball.uz.screens.drawers
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,10 +25,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import com.yandex.maps.mobile.BuildConfig
 import goball.uz.R
 import goball.uz.helper.FinishActivityState
 
@@ -30,19 +40,18 @@ class AboutUsScreen() : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
-
         Scaffold(modifier = Modifier.fillMaxSize(),
             topBar = {
                 TopAppBar(title = {
-                        Text(
-                            text = "Biz haqimizda",
-                            modifier = Modifier.padding(start = 9.dp),
-                            style = MaterialTheme.typography.bodyMedium
-                        )
+                    Text(
+                        text = "Biz haqimizda",
+                        modifier = Modifier.padding(start = 9.dp),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
 
                 },
                     navigationIcon = {
-                        IconButton(onClick = { FinishActivityState.shouldFinish.value = true  }) {
+                        IconButton(onClick = { FinishActivityState.shouldFinish.value = true }) {
                             Icon(
                                 painter = painterResource(id = R.drawable.arrow_back),
                                 contentDescription = "Go back",
@@ -52,17 +61,76 @@ class AboutUsScreen() : Screen {
                     }
                 )
             }
-        ) {
+        ) { paddingValue ->
             Column(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValue),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                Text(text = "About Us page")
-                Spacer(modifier = Modifier.height(50.dp))
+                val version = BuildConfig.VERSION_NAME
+                val lastUpdatedDate = "28.06.2024"
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Version $version", style = MaterialTheme.typography.bodySmall)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(text = lastUpdatedDate, style = MaterialTheme.typography.bodySmall)
+                }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Condition of service use",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = colorResource(
+                            id = R.color.primary
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Privacy policy",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = colorResource(
+                            id = R.color.primary
+                        )
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.send_message_icon),
+                            contentDescription = "Message",
+                            Modifier.padding(6.dp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(18.dp))
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.rate_app),
+                            contentDescription = "Rate",
+                            Modifier.padding(6.dp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(18.dp))
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.share_icon_44),
+                            contentDescription = "Share",
+                            Modifier.padding(4.dp)
+                        )
+                    }
+                }
+
             }
 
         }
-        }
+    }
 
 }
